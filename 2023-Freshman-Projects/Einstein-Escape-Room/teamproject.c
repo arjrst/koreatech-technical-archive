ec1356088a441a180888e2bbd9501519678bced4
Arzu Rustamova <151707798+arjrst@users.noreply.github.com>
2026-02-16 18:34:38 +0900

    Add files via upload

---
+++ 2023-Freshman-Projects/Einstein-Escape-Room/teamproject.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int main() { 
+	printf("You regain your consciousness and you are in a strange room.");
+	while (getchar() != '\n');
+	printf("There is a card infront of you.");
+	while (getchar() != '\n');
+	printf("\n-----------------------------------------------");
+	printf("\n\nYour name is Einstein\n");
+	printf("\n\nGuess the quizes in each room.\n");
+	printf("\n\nEverytime you solve the quiz \nyou're going to recover your memory one by one.\n");
+	printf("\n-----------------------------------------------");
+
+
+}
